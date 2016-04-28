c35e803b391c29b0444dcc9482bc2868381cf706
Ivan Coronado <ivacormor@gmail.com>
2016-04-29 00:05:39 +0200

    feature(data.service): data functions moved to dataService

--- src/app/components/data.service.js
+++ src/app/components/data.service.js
@@ -1,20 +1,84 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('aurora')
-    .factory('dataService', dataServiceFactory);
+    angular
+      .module('aurora')
+      .factory('dataService', dataServiceFactory);
 
-  /* @ngInject */
-  function dataServiceFactory() {
-    var service = {
-      getData: getData
-    };
+    /* @ngInject */
+    function dataServiceFactory($q) {
+        var service = {
+            getData: getData,
+            getTableCompatibleData: getTableCompatibleData,
+            getNvd3CompatibleData: getNvd3CompatibleData
+        };
 
-    return service;
+        return service;
 
-    function getData() {
-      
+        function getData() {
+            var deferred = $q.defer();
+  
+            d3.csv("/assets/data/Data2.csv")
+                .get(function(error, rows) { 
+                    if(angular.isObject(error)){
+                        deferred.reject(error);
+                    }else{
+                        deferred.resolve(rows)
+                    } 
+                });
+
+            return deferred.promise;
+        }
+
+        function getTableCompatibleData(data){
+            var keys, values, tableData;
+            keys = _.chain(data).first().keysIn().value();
+            values = _.chain(data).map(function(row){
+                return _.values(row);
+            }).value();
+
+            tableData = {
+                head : keys,
+                values : values
+            }
+            
+            return tableData;
+        }
+
+        function getNvd3CompatibleData(data) {
+            var keys, series;
+
+            keys = _.chain(data).first().omit('Date').keysIn().value();
+                                   
+            series = getSeriesKeys();
+            initSeriesValues(series, data);
+
+            return series;
+
+            function getSeriesKeys(){
+                var temporalSeries = [];
+
+                _.map(keys, function(key){
+                    var serie = {
+                        key: key,
+                        values: []
+                    };
+                    temporalSeries.push(serie);
+                })
+                return temporalSeries;
+            }
+
+            function initSeriesValues(series, data){
+                _.map(data, function(row){
+                    _.map(series, function(serie){
+                        var seriePoint = {
+                            x: moment(row.Date, "DD/MM/YYYY").add(7,'days').toDate(),
+                            y: _.chain(row).get(serie.key).replace(',','.').parseInt().value()
+                        };
+                        serie.values.push(seriePoint);
+                    })
+                })
+            }
+        }
     }
-  }
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,79 +1,48 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('aurora')
-    .controller('MainController', MainController);
+    angular
+        .module('aurora')
+        .controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController() {
-  	var vm = this;
-    d3.csv("/assets/data/Data2.csv")
-        .get(function(error, rows) { vm.data = parseToNvd3CompatibleData(rows) });
+    /** @ngInject */
+    function MainController(dataService) {
+        var vm = this,
+        format = d3.time.format("%m/%d/%Y");
 
-        function parseToNvd3CompatibleData(data) {
-                var keys, series;
+        init();
 
-                keys = _.chain(data).drop().first().omit('Date').value();
-                                               
-                series = getSeriesKeys();
-                initSeriesValues(series, data);
-
-                console.log(series);
-                return series;
-
-                function getSeriesKeys(){
-                    var temporalSeries = [];
-
-                    _.forEach(keys, function(key, index){
-                        var serie = {
-                            key: index,
-                            values: []
-                        };
-                        temporalSeries.push(serie);
-                    })
-                    return temporalSeries;
-                }
-
-                function initSeriesValues(series, data){
-                    _.map(data, function(row){
-                        _.map(series, function(serie){
-                            var seriePoint = {
-                                x: moment(row.Date, "DD/MM/YYYY").add(7,'days').toDate(),
-                                y: _.chain(row).get(serie.key).replace(',','.').parseInt().value()
-                            };
-                            serie.values.push(seriePoint);
-                        })
-                    })
-                }
-            }
-
-    var format = d3.time.format("%m/%d/%Y");
-    vm.options = {
-        chart: {
-            type: 'lineChart',
-            height: 450,
-            margin : {
-                top: 20,
-                right: 20,
-                bottom: 60,
-                left: 55
-            },
-            x: function(d){ return new Date(d.x); },
-            y: function(d){ return d.y; },
-            useInteractiveGuideline: true,
-            xAxis: {
-                axisLabel: 'Day'
-            },
-            yAxis: {
-                axisLabel: 'Consumo',
-                tickFormat: function(d){
-                    return d3.format('.02f')(d);
-                },
-                axisLabelDistance: -10
-            }
+        function init(){
+            dataService.getData().then(function(data){
+                vm.nvd3Data = dataService.getNvd3CompatibleData(data);
+                vm.tableData = dataService.getTableCompatibleData(data); 
+            });
         }
-    };
-      
-  }
+
+        vm.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 55
+                },
+                x: function(d){ return new Date(d.x); },
+                y: function(d){ return d.y; },
+                useInteractiveGuideline: true,
+                xAxis: {
+                    axisLabel: 'Day'
+                },
+                yAxis: {
+                    axisLabel: 'Consumo',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: -10
+                }
+            }
+        };
+    }
 })();
